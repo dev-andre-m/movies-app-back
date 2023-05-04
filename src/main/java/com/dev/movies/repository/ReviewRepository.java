1e399e019c509deaa66598755bf6e5c74779776a
Andre Mendes <andre.eletrom@hotmail.com>
2023-05-04 11:21:48 -0300

    feat: added create review endpoint

---
+++ src/main/java/com/dev/movies/repository/ReviewRepository.java
@@ -0,0 +1,10 @@
+package com.dev.movies.repository;
+
+import com.dev.movies.domain.Review;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
